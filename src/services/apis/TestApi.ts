0b30427bfc99bd53171cd43d9e13c39890b90f4c
Syed Salman Qadri <salqadri@gmail.com>
2019-01-10 11:38:39 -0800

    Update with express

---
+++ src/services/apis/TestApi.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+// declare module 'TestApi' {
+//     class TestApi {
+//         getFoo () {
+//             axios.get('/api/foo').then((response) => {
+//                 console.log(response.data);
+//             });
+//         }
+//     }
+//     export = TestApi
+// }
+
+declare module 'rivescript' {
+    class rivescript {
+      constructor()
+    }
+    export = RiveScript;
+  }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,17 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import TestApi from '@/services/apis/TestApi.js';
 
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  created () {
+    let testApi = new TestApi();
+    testApi.getFoo();
+  }
+}
 </script>
